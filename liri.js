2037148d35736c2fd6c168092a82f4aa3bafcb4b
Damien Wright <damien@Damiens-MacBook-Pro.local>
2016-09-03 22:08:06 -0700

    file now pulls in twitter keys and saves them to a variable

--- liri.js
+++ liri.js
@@ -1,5 +1,11 @@
-var myKeys = require('request');
+var myKeys = require('./keys.js');
 
-var twitter = myKeys.twitterKeys;
+var consumer_key = myKeys.twitterKeys.consumer_key;
+var consumer_secret = myKeys.twitterKeys.consumer_secret;
+var access_key = myKeys.twitterKeys.access_token_key;
+var access_secret = myKeys.twitterKeys.access_token_secret;
 
-console.log("Object name: " + twitter);
+console.log("Consumer Key: " + consumer_key);
+console.log("Consumer Secret: " + consumer_secret);
+console.log("Access Token Key: " + access_key);
+console.log("Access Token Secret: " + access_secret);
